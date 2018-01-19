5a69e3796b29f046b26daa9dbd5e6d9c4286473e
jwilder4690 <thatoneguy4690@gmail.com>
2018-01-18 21:30:51 -0500

    Added Typing Game page
    -Created script to accept user input.
    -Imported p5.dom
    -Updated link from projects page

--- test2/test2.js
+++ test2/test2.js
@@ -1,3 +1,5 @@
+var canonVector;
+
 function setup() {
   if (width == displayWidth)
   {
@@ -7,14 +9,20 @@ function setup() {
   {
     createCanvas(windowWidth, windowWidth*(9/16));
   }
-  
+  canonVector = createVector(70,height-150);
   background(0);
 }
 
 function draw() {
     stroke(150,150,150);
     fill(255);
-    rect(mouseX-25, mouseY-25,50,50);
+    //rect(mouseX-25, mouseY-25,50,50);
+    //push();
+    //translate(canonVector.x, canonVector.y);
+    //rotate(PI/4);  
+    rect(canonVector.x, canonVector.y,100,100);
+    print(canonVector.x +", "+canonVector.y);
+    //pop();
 }
 
 function windowResized(){
---
+++ typingGame/p5js-temp-typingGame1199058870919535291.js
@@ -0,0 +1,31 @@
+var inp;
+var counter = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowWidth*(9/16));
+  background(0);
+  var inp = createInput();
+  inp.position(width/2,65);
+  inp.input(myInputEvent);
+}
+
+function myInputEvent() {
+  console.log('you are typing: ', this.value()); 
+  counter++;
+}
+
+function draw() {
+  background(counter);
+}
+
+function windowResized(){
+  if (width == displayWidth)
+  {
+    resizeCanvas(windowWidth-19, (windowWidth-19)*(9/16));
+  }
+  else
+  {
+    resizeCanvas(windowWidth, windowWidth*(9/16));
+  }
+  background(0);
+}
