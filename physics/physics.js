170acd01cb9ee697879ccf7b817c4c33b059b163
jwilder4690 <thatoneguy4690@gmail.com>
2018-02-14 21:52:51 -0500

    Fixed Bouce

--- physics/physics.js
+++ physics/physics.js
@@ -1,8 +1,9 @@
 var ballSpacing = 50;
 var ballSize = 15;
 var balls = [];
-var hero;
-var velocity = 1/30;
+var heros = [];
+var velocity = 1.0/30;
+var resistance = 0.00007;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,7 +11,7 @@ function setup() {
   var cols = windowWidth/ballSpacing;
   var rows = windowHeight/ballSpacing;
   var counter = 0;
-  hero = new Ball(10, 10, 1, 1, ballSize);
+  heros[0] = new Ball(10, 10, 1, 1, ballSize);
   for (var i = 1; i < rows; i++){
     for(var j = 1; j < cols; j++){
       if(i % 2 == 0){
@@ -27,13 +28,16 @@ function setup() {
 function draw() {
   background(100);
   fill(255,255,255);
-  for(var i = 0; i < balls.length; i++)
-  {
-    balls[i].drawBall();
-  }
+  //for(var i = 0; i < balls.length; i++)
+  //{
+  //  balls[i].drawBall();
+  //}
   fill(0,255,0);
-  hero.drawBall();
-  hero.update();
+  for(var i = 0; i < heros.length; i++){
+    heros[0].drawBall();
+    heros[0].update();
+    heros[0].checkCollision(balls);
+  }
 }
 
 function Ball(x, y, xv, yv, size){
@@ -55,29 +59,57 @@ function Ball(x, y, xv, yv, size){
   this.yPos = mouseY;
  }
  this.changeVelocity = function(){
-   this.xVel = velocity*(this.xPos - mouseX);
-   this.yVel = velocity*(this.yPos - mouseY);
+   this.xVel = (this.xPos - mouseX);
+   this.yVel = (this.yPos - mouseY);
  }
  
  this.update = function(){
-    this.xPos += this.xVel;
-    this.yPos += this.yVel;
-    if(this.xPos - this.size <= 0 || this.xPos + this.size >= windowWidth){
+    if(this.xPos - this.size <= 0){
+      this.xPos = this.size;
       this.xVel = -this.xVel;
+      velocity = velocity/1.1
     }
-    if(this.yPos - this.size <= 0 || this.yPos + this.size >= windowHeight){
+    if(this.xPos + this.size >= windowWidth){
+      this.xPos = windowWidth - this.size;
+      this.xVel = -this.xVel;
+      velocity = velocity/1.1
+    }
+    if(this.yPos - this.size <= 0){
+      this.yPos = this.size;
       this.yVel = -this.yVel;
+      velocity = velocity/1.1      
     }
+    if(this.yPos + this.size >= windowHeight){
+      this.yPos = windowHeight - this.size;
+      this.yVel = -this.yVel;
+      velocity = velocity/1.1
+    }
+    
+   velocity -= resistance;
+   if (velocity < 0) {
+     velocity = 0;
+   }
+    
+   this.xPos += this.xVel*velocity;
+   this.yPos += this.yVel*velocity;
+   
+
+
+ }
+ 
+ this.checkCollision = function(obstacles){
+  //TODO: LOOP THROUGH OBSACLES AND ADJUST HERO VELOCITY IF COLLISION IS DETECTED
  }
 }
 
 
 function mousePressed(){
-  hero.moveHero();
+  velocity = 1/30;
+  heros[0].moveHero();
 }
 
 function mouseReleased(){
-  hero.changeVelocity();
+  heros[0].changeVelocity();
 }
 
 function windowResized(){
